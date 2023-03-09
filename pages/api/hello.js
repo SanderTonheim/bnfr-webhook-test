a7810a510b15cc2104b3763a111d1ef471be288d
Sander Tonheim <sandert.kodehode@gmail.com>
2023-03-09 13:50:45 +0100

    algolia index update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,31 +3,20 @@ import groq from 'groq'
 import sanityClient from '../../client'
 import client from '../../client'
 
-export async function getServerSideProps() {
-	const algoliasearch = require('algoliasearch')
-	const client = algoliasearch('BC0Z4HS7B1', '285796df216a570721c98b33ba986821')
-	const index = client.initIndex('Members')
-	const response = await sanityClient.fetch(groq`*[_type == "medlem"]`).then((item) => {
-		item.map((item) => {
+export default async function handler(req, res) {
+	const data = await client.fetch(groq`*[_type == "medlem"]`)
+	console.log(data)
+
+	try {
+		const indexList = data.map((item) => {
+			const algoliasearch = require('algoliasearch')
+			const client = algoliasearch('BC0Z4HS7B1', '285796df216a570721c98b33ba986821')
+			const index = client.initIndex('Members')
 			const record = { objectID: item._id, name: item.name }
 			index.saveObject(record).wait()
-			console.log(item)
+			return item
 		})
-		return response
-	})
-
-	return {
-		props: {
-			response,
-		},
-	}
-}
-
-export default async function handler(req, res) {
-	try {
-		const result = await getServerSideProps()
-
-		res.status(200).json({ result })
+		res.status(200).json(indexList)
 	} catch (err) {
 		res.status(500).json({ error: 'failed to load data' })
 	}
