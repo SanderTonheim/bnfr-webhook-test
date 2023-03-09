af9eb97cc91981846c7595fad8ab07fa031ad47c
Sander Tonheim <sandert.kodehode@gmail.com>
2023-03-09 13:24:26 +0100

    api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,22 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import groq from 'groq'
 import sanityClient from '../../client'
+import client from '../../client'
 
-export default function handler(req, res) {
-	// get data from cms
-
-	const data = sanityClient.fetch(groq`*[_type == "medlem"]`).then((response) => {
-		const algoliasearch = require('algoliasearch')
-		const client = algoliasearch('BC0Z4HS7B1', '285796df216a570721c98b33ba986821')
-		const index = client.initIndex('Members')
-
-		response.map((item) => {
+export async function getServerSideProps() {
+	const algoliasearch = require('algoliasearch')
+	const client = algoliasearch('BC0Z4HS7B1', '285796df216a570721c98b33ba986821')
+	const index = client.initIndex('Members')
+	const response = await sanityClient.fetch(groq`*[_type == "medlem"]`).then((item) => {
+		item.map((item) => {
 			const record = { objectID: item._id, name: item.name }
 			index.saveObject(record).wait()
 			console.log(item)
 		})
+		return response
 	})
-	console.log(data)
 
-	res.status(200).json('data')
+	return {
+		props: {
+			response,
+		},
+	}
+}
+
+export default async function handler(req, res) {
+	try {
+		const result = await getServerSideProps()
+
+		res.status(200).json({ result })
+	} catch (err) {
+		res.status(500).json({ error: 'failed to load data' })
+	}
 }
